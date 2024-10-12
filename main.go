65d2d5cc0593966490900557c18bd731a24f8ab6
Александр Калинин <bach8616@gmail.com>
2024-10-12 10:04:45 +0300

    create start server

--- main.go
+++ main.go
@@ -2,4 +2,11 @@ package main
 
 func main() {
 	println("usage: make test")
+	//ctx, _ := context.WithCancel(context.Background())
+	//
+	//err := StartMyMicroservice(ctx, ":8081", "")
+	//
+	//if err != nil {
+	//	return
+	//}
 }
--- service.go
+++ service.go
@@ -1,14 +1,35 @@
 package main
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
 
 func StartMyMicroservice(ctx context.Context, listenAddr string, aclData string) error {
 
-	select {
-	case <-ctx.Done():
+	var accessData map[string][]string
 
-	default:
+	err := json.Unmarshal([]byte(aclData), &accessData)
+	if err != nil {
+		return err
 	}
+	lis, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		log.Fatalln("Cant listen port", err)
+	}
+	server := grpc.NewServer()
+
+	RegisterAdminServer(server, &UnimplementedAdminServer{})
+
+	go func() {
+		go server.Serve(lis)
+		<-ctx.Done()
+		server.Stop()
+		lis.Close()
+	}()
 
 	return nil
 }
